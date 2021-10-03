8527f267daa56ca5e7a96e899897e10b881d2fe4
Guilherme Torres <guiatorres8@gmail.com>
2021-10-03 01:07:06 -0300

    finish lesson 2.5

--- src/store/store.js
+++ src/store/store.js
@@ -17,10 +17,21 @@ export default new Vuex.Store({
       'community',
     ],
     events: [],
+    event: Object,
+    totalEvents: Number,
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENT(state, event) {
+      state.event = event
+    },
+    SET_TOTAL_EVENTS(state, totalEvents) {
+      state.totalEvents = totalEvents
     },
   },
   actions: {
@@ -28,6 +39,33 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then((response) => {
+          commit(
+            'SET_TOTAL_EVENTS',
+            parseInt(response.headers['x-total-count'])
+          )
+          commit('SET_EVENTS', response.data)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      let event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then((response) => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      }
     },
   },
   modules: {},
