665c33d5119131a0cf6f9f90797df049a43bc44b
Guilherme Torres <guiatorres8@gmail.com>
2021-10-02 21:39:12 -0300

    finish lesson 3

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Create event</h1>
+  <div>
+    <h1>Create event, {{ user.name }}</h1>
+    <p>This event was created by the user with the id: {{ user.id }}</p>
+    <p>{{ getEventById(1) }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories']),
+  },
+}
 </script>
 
 <style></style>
